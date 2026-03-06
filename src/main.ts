7ee4fa4baa6dc806905400e0c2858e76ce217e52
Tomiwa Adelae <adelaetomiwa6@gmail.com>
2026-03-06 18:19:33 +0100

    Updated the cors

--- src/main.ts
+++ src/main.ts
@@ -24,19 +24,27 @@ async function bootstrap() {
     'http://192.168.0.109:3000',
   ].filter(Boolean);
 
-  app.enableCors({
-    origin: (origin, callback) => {
-      // Allow requests with no origin (like mobile apps or curl)
-      if (!origin) return callback(null, true);
+  // app.enableCors({
 
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+  //   origin: (origin, callback) => {
+  //     // Allow requests with no origin (like mobile apps or curl)
+  //     if (!origin) return callback(null, true);
+
+  //     if (allowedOrigins.includes(origin)) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  //   credentials: true,
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  // });
+  app.enableCors({
+    origin: 'https://www.ekovibe.com.ng',
+    // process.env.FRONTEND_URL ||
+    // 'http://localhost:3001' ||
+    // 'http://192.168.1.250:3000',
     credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
   await app.listen(process.env.PORT ?? 3000);
